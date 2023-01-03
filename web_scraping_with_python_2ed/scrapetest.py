df6b7ac0b8455fb1a678bfbd3f7493c2f9c22d5e
Tom Madsen <tmadutah@gmail.com>
2023-01-02 22:01:22 -0700

    web scraping with python thru chap 1

--- web_scraping_with_python_2ed/scrapetest.py
+++ web_scraping_with_python_2ed/scrapetest.py
@@ -1,8 +1,23 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen('http://pythonscraping.com/pages/page2.html')
-bs = BeautifulSoup(html, 'html.parser')
-print(bs.h1)
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        # title = bs.head.title.text
+        title = bs.body.h1.text
+    except AttributeError as e:
+        return None
+    return title
 
+title = getTitle('http://pythonscraping.com/pages/page2.html')
+if title == None:
+    print('Title could not be found')
+else:
+    print(title)
 # pg10 - Connecting Reliably and Handling Exceptions
